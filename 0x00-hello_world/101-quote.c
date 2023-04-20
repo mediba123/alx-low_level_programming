800245fbd60cf6887d770598b5c30b198cb02733
orient dev <edwinabandoh2@gmail.com>
2023-04-20 13:22:01 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
-int main(void) {
-    const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, msg, 60);
-    return 1;
-}
 
+/**
+ * main-Entry point
+ *
+ * Return: Always 1 (success).
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
