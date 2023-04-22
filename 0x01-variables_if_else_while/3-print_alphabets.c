5a8addfc24fd92f2bb4e33283330c703811966a8
orient dev <edwinabandoh2@gmail.com>
2023-04-21 18:14:52 -0700

    print alpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
+
 #include <ctype.h>
-
-int main() {
-    int letter;
-    for (letter = 'a'; letter <= 'z'; letter++) {
-        putchar(tolower(letter));
-        if (letter == 'z') {
-            for (letter = 'A'; letter <= 'Z'; letter++) {
-                putchar(letter);
-            }
-            break;
-        }
-    }
-    putchar('\n');
-    return 0;
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * positive,negative or zero
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+alphabet = tolower(alphabet);
+putchar(alphabet);
+if (alphabet == 'z')
+{
+alphabet = 'A';
+for (; alphabet <= 'Z'; alphabet++)
+{
+putchar(alphabet);
 }
-
+break;
+}
+}
+putchar('\n');
+return (0);
+}
