55abec11e729b516c996621477ebf4369c96739f
orient dev <edwinabandoh2@gmail.com>
2023-04-24 14:21:07 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *Main -Entry point
+ *
+ *success - 0
+ */
+int main (void)
+{
+int number;
+      for(number = "0"; number <= "9"; number++)
+	      putchar(number);
+      {
+		      if (number =="9");
+		      continue;
+		      putchar(",");
+		      putchar(" ");	   ;   
+        }
+      putchar("\n")
+	      return(0)
+
+
+
+
+}
