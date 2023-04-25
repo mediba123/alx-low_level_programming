c7d6c90658ebfb78fcf42f4f759cd86a1c98aea9
orient dev <edwinabandoh2@gmail.com>
2023-04-25 03:07:59 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *Main -point entry
+ *
+ *Return : 0
+ */
+int main (void)
+{
+	 _putchar('-');
+	 _putchar('p');
+	 _putchar('u');
+	 _putchar('t');
+	 _putchar('c');
+	 _putchar('h');
+	 _putchar('a');
+	 _putchar('r');
+
+	return 0;
+}	
