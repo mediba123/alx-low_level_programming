ec00eaa0bd6b44ce2a4326d3b4ced025e3ec55b5
orient dev <edwinabandoh2@gmail.com>
2023-04-25 08:43:19 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,33 +4,33 @@
 
 #include <stdlib.h>
 /**
- * main - main type
- * Description: Get a random number 
+ * main - main block
+ * Description: Get a random number and print the number
  * positive,nagetive or zero
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	int n;
-	int lat;
+	int last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last = n % 10;
 
-	if (lat == 0)
+	if (last == 0)
 	{
-		printf("Lat digit of %d is %d and is 0\n", n, lat);
+		printf("Last digit of %d is %d and is 0\n", n, last);
 	}
 	else
 	{
-		if (lat > 5)
+		if (last > 5)
 		{
-			printf("Lat digit of %d is %d and is greater than 5\n", n, lat);
+			printf("Last digit of %d is %d and is greater than 5\n", n, last);
 		}
 		else if (last < 6)
 		{
-			printf("Lat digit of %d is %d and is less than 6 and not 0\n", n, lat);
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 		}
 	}	return (0);
 }
