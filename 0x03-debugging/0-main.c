160bd22b2cf73fb777b22862b3fc0871ff845504
orient dev <edwinabandoh2@gmail.com>
2023-04-26 10:08:24 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-#include <stdio.h>
+/**
+ * main - Debugging.
+ *
+ * Return: 0
+ */
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
+int main(void)
+{
+	int i;
 
-#endif /* MAIN_H */
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
