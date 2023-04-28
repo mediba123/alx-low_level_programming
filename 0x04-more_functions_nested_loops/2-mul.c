8fc6dc78ad425dcb3edae572b4f3bbd171109fb7
orient dev <edwinabandoh2@gmail.com>
2023-04-28 12:55:00 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *mul - multiples two integers
+ *@a: 1st int should be * by b
+ *@b: 2nd int should be * by a
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	d = (a * b);
+
+		return (d);
+}
