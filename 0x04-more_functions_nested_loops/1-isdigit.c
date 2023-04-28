4ffdf5f617ab0973abe237c244258b4d024dc9ce
orient dev <edwinabandoh2@gmail.com>
2023-04-28 12:19:11 -0700

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_isdigit.c - check for a digit (0 through 9).
+ * @c: interger should be check
+ *Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+
+}
+
