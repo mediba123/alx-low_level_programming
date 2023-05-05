5b0f95cc77bdbbb68e33a718d58f9bfbfe9a5b95
orient dev <edwinabandoh2@gmail.com>
2023-05-04 18:42:52 -0700

    cat str

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,7 +14,7 @@ char *_strncat(char *dest, char *src, int n)
 /* j is a counter for  n bytes of src to be concatenated */
 /* length = length of destination string */
 
-	length = 0;
+	th = 0;
 	while (dest[th] != '\0')
 	{
 		th++;
