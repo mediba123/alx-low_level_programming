d2ed0033739f4df9e56105d4f275089f4afd9965
orient dev <edwinabandoh2@gmail.com>
2023-05-04 18:41:17 -0700

    str cat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *_strncat - function that concatenates two strings. it will use
+ * at most n bytes from src. src does not need to be null terminated.
+ *@dest: pointer to destination string.
+ *@src: pointer to source string.
+ *@n: number of bytes to be concatenated.
+ *
+ *Return: pointer to destination string.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int th, j;
+/* j is a counter for  n bytes of src to be concatenated */
+/* length = length of destination string */
+
+	length = 0;
+	while (dest[th] != '\0')
+	{
+		th++;
+	}
+	for (j = 0; j < n && src[j] != '\0'; j++, th++)
+	{
+		dest[th] = src[j];
+	}
+	dest[th] = '\0';
+	return (dest);
+}
